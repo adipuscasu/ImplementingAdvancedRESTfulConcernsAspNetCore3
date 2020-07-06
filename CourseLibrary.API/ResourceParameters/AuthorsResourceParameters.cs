a70f16acd3a8a3c1d965dc092d58e587861b130e
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2020-07-07 00:03:54 +0300

    work in progress for adding pagination

--- CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
+++ CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
@@ -2,7 +2,16 @@
 {
     public class AuthorsResourceParameters
     {
+        private const int MaxPageSize = 20;
+        private int _pageSize = 10;
         public string MainCategory { get; set; }
         public string SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
     }
 }
