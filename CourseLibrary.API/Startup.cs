e17ef2276289faebecee420f058ffe62e90650a0
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2020-07-06 20:57:00 +0300

    adds Swagger

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -91,6 +91,7 @@ namespace CourseLibrary.API
                 options.UseSqlServer(
                     @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
             }); 
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -112,6 +113,16 @@ namespace CourseLibrary.API
                 });
 
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CourseLibrary.API V1");
+            });
 
             app.UseRouting();
 
