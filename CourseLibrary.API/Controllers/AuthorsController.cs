3b586fc95bd5e0a90c72fc72420ea107ecc35c73
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2020-07-25 23:31:54 +0300

    adds the root controller

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -109,7 +109,7 @@ namespace CourseLibrary.API.Controllers
             return Ok(linkedResourceToReturn);
         }
 
-        [HttpPost]
+        [HttpPost(Name = "CreateAuthor")]
         public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
         {
             var authorEntity = _mapper.Map<Entities.Author>(author);
---
+++ CourseLibrary.API/Controllers/RootController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using CourseLibrary.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = "GetRoot")]
+        public IActionResult GetRoot()
+        {
+            // create links for root
+            var links = new List<LinkDto>();
+
+            links.Add(
+                new LinkDto(Url.Link("GetRoot", new {}),
+                    "self",
+                    "GET"));
+
+            links.Add((new LinkDto(Url.Link("GetAuthors", new {}),
+                "authors",
+                "GET")));
+
+            links.Add(
+                new LinkDto(Url.Link("CreateAuthor", new {}),
+                    "create_author",
+                    "POST"));
+
+            return Ok(links);
+        }
+    }
+}
